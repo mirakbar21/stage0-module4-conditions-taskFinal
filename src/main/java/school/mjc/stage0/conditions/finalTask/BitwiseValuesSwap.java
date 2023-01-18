03ec8bf7ab1c3ceed22169da53049c1bc66371dc
Mirakbar Mirzokhidov <mirakbarmirzokhidov21@gmail.com>
2023-01-19 02:34:46 +0600

    done: final task

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        first = first ^ second;
+        second = second ^ first;
+        first = second ^ first;
+        System.out.println(first);
+        System.out.println(second);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,72 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (month < 1 || month > 12) {
+            System.out.println("invalid date");
+        } else if (year % 4 == 1) {
+            switch (month) {
+                case 2:
+                    System.out.println("28");
+                    break;
+                case 1, 3, 5, 7, 8, 10, 12:
+                    System.out.println("31");
+                    break;
+                case 4, 6, 9, 11:
+                    System.out.println("30");
+                    break;
+                default:
+                    System.out.println("invalid date");
 
+            }
+        } else if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    switch (month) {
+                        case 2:
+                            System.out.println("29");
+                            break;
+                        case 1, 3, 5, 7, 8, 10, 12:
+                            System.out.println("31");
+                            break;
+                        case 4, 6, 9, 11:
+                            System.out.println("30");
+                            break;
+                        default:
+                            System.out.println("invalid date");
+                            break;
+                    }
+                } else {
+                    switch (month) {
+                        case 2:
+                            System.out.println("28");
+                            break;
+                        case 1, 3, 5, 7, 8, 10, 12:
+                            System.out.println("31");
+                            break;
+                        case 4, 6, 9, 11:
+                            System.out.println("30");
+                            break;
+                        default:
+                            System.out.println("invalid date");
+                    }
+                }
+            } else if (year % 100 != 0) {
+                switch (month) {
+                    case 2:
+                        System.out.println("29");
+                        break;
+                    case 1, 3, 5, 7, 8, 10, 12:
+                        System.out.println("31");
+                        break;
+                    case 4, 6, 9, 11:
+                        System.out.println("30");
+                        break;
+                    default:
+                        System.out.println("invalid date");
+                        break;
+                }
+
+            }
+        }
     }
 }
